d37ff466cb5cdf2baa8e429dabed2452d84fbc19
Shiliang Zhang <lsftw13@gmail.com>
2014-11-13 17:08:50 -0500

    Added clicking and hovering for buttons.

--- js/hud.js
+++ js/hud.js
@@ -10,17 +10,43 @@ var Ui = {
 
 // TODO get HUD working
 
-function makeButton(x, y, imageCategory, imageName) {
+function makeButton(x, y, imageCategory, imageName, onClickFunction) {
     var button = {
         x: x,
         y: y,
         image: Images.getImage(imageCategory, imageName),
-        checkClicked: function(mouseX, mouseY) {
+        hovered: false, // whether mouse is hovering over this
+        selected: false, // whether mouse clicked this
+        tryMouseOver: function(mouseX, mouseY) {
+            this.hovered = this.isInButton(mouseX, mouseY);
+        },
+        tryClick: function(mouseX, mouseY) {
+            this.selected = this.isInButton(mouseX, mouseY);
+            if (this.selected) {
+                onClickFunction();
+            }
+        },
+        isInButton: function(mouseX, mouseY) {
             return mouseX >= this.x && mouseX <= this.x + this.width
                 && mouseY >= this.y && mouseY <= this.y + this.height;
         },
         draw: function(ctx) {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            if (this.selected) {
+                var oldLineWidth = ctx.lineWidth;
+                ctx.beginPath();
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = "orange";
+                var off = ctx.lineWidth / 2;
+                ctx.rect(this.x + off, this.y + off, this.width - off*2, this.height - off*2);
+                ctx.stroke();
+                ctx.lineWidth = oldLineWidth;
+            } else if (this.hovered) {
+                ctx.beginPath();
+                ctx.strokeStyle = "yellow";
+                ctx.rect(this.x, this.y, this.width, this.height);
+                ctx.stroke();
+            }
         }
     };
     button.width = button.image.width;
@@ -34,6 +60,33 @@ function drawHud(ctx) {
     });
 }
 
+function addMenuButtons() {
+    var placeFireballTower = makeButton(14 * TILE_WIDTH, 0, 'tower', 'fireball.png',
+        function() {
+            console.log('fireball selected!');
+        });
+    Ui.buttons.push(placeFireballTower);
+}
+
+function addEventListeners(canvas) {
+    canvas.addEventListener('click', function(event) {
+        var mouseX = event.pageX - canvas.offsetLeft;
+        var mouseY = event.pageY - canvas.offsetTop;
+        Ui.buttons.forEach(function(button) {
+            button.tryClick(mouseX, mouseY);
+        });
+    });
+    canvas.addEventListener('mousemove', function(event) {
+        var mouseX = event.pageX - canvas.offsetLeft;
+        var mouseY = event.pageY - canvas.offsetTop;
+        Ui.buttons.forEach(function(button) {
+            button.tryMouseOver(mouseX, mouseY);
+        });
+    });
+}
+
+// Call this once when game starts to initialize hud
 function initHud() {
-    Ui.buttons.push(makeButton(14 * TILE_WIDTH, 0, 'tower', 'fireball.png'));
+    addMenuButtons();
+    addEventListeners(canvas);
 }
