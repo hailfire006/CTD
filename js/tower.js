82a26d0a77405b05f805f486d8524c8964f23b98
Shiliang Zhang <lsftw13@gmail.com>
2014-11-18 10:41:14 -0500

    Added helper function for tower to get the coords of a tile relative to current position.

--- js/tower.js
+++ js/tower.js
@@ -11,19 +11,27 @@ function makeTower(gx, gy, imageName, range, coolDown) {
     var tower = makeEntity(gx, gy, imageCategory, imageName);
     tower.coolDown = coolDown;
     tower.coolDownTimer = 0;
+	// tower helper functions
+	// Returns (gx, gy) for the tile offset relative to tower's tile
+	tower.getRelativeTileCoords = function(tileOffsetX, tileOffsetY) {
+        var towerTile = grid.graphicalToTileCoords(tower.gx,tower.gy);
+        var targetTilex = towerTile.tx + tileOffsetX;
+        var targetTiley = towerTile.ty + tileOffsetY;
+        var targetX = targetTilex * TILE_WIDTH; 
+        var targetY = targetTiley * TILE_HEIGHT;
+		return {gx: targetX, gy: targetY};
+	};
+	// tower functions
     tower.upgrade = function () {
         // TODO upgrade stats, change image
     };
     tower.fire = function () {
         var towerTile = grid.graphicalToTileCoords(tower.gx,tower.gy);
         // fire to the right
-        var targetTilex = towerTile.tx + 6;
-        var targetTiley = towerTile.ty + 7;
-        var targetx = targetTilex * TILE_WIDTH; 
-        var targety = targetTiley * TILE_HEIGHT;
-        var projectile = makeTestProjectile(this,targetx,targety);
+		var targetCoords = this.getRelativeTileCoords(6, 7);
+        var projectile = makeTestProjectile(this, targetCoords.gx, targetCoords.gy);
         grid.addEntity(projectile);
-    }
+    };
     tower.update = function (mod) {
         tower.coolDownTimer -= mod;
         if (tower.coolDownTimer <= 0) {
