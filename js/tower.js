41533bbec4b3783d32a1a5a0cd6065904efa3d9f
Jung Kim <jamwjam@gmail.com>
2014-11-19 18:49:52 -0500

    Minor change

--- js/tower.js
+++ js/tower.js
@@ -110,28 +110,28 @@ function makeLightningTower(gx,gy) {
     return tower;
 }
 function makeMagicTower(gx,gy) {
-    var tower = makeTower(gx,gy,"lightningbolt.png",4,1.5);
+    var tower = makeTower(gx,gy,"magicTower.png",4,1.5);
     tower.makeProjectile = function (gx, gy) {
         return makeLightningProjectile(tower, gx, gy)
     };
     return tower;
 }
 function makeSpikyGemTower(gx,gy) {
-    var tower = makeTower(gx,gy,"lightningbolt.png",4,1.5);
+    var tower = makeTower(gx,gy,"spikyGemTower.png",4,1.5);
     tower.makeProjectile = function (gx, gy) {
         return makeLightningProjectile(tower, gx, gy)
     };
     return tower;
 }
 function makeSpookyTower(gx,gy) {
-    var tower = makeTower(gx,gy,"lightningbolt.png",4,1.5);
+    var tower = makeTower(gx,gy,"spookyTower.png",4,1.5);
     tower.makeProjectile = function (gx, gy) {
         return makeLightningProjectile(tower, gx, gy)
     };
     return tower;
 }
 function makeKingTower(gx,gy) {
-    var tower = makeTower(gx,gy,"lightningbolt.png",4,1.5);
+    var tower = makeTower(gx,gy,"kingTower.png",4,1.5);
     tower.makeProjectile = function (gx, gy) {
         return makeLightningProjectile(tower, gx, gy)
     };
