d304f0f5a7bc5142ea37c7787dd782f3edf282cd
Shiliang Zhang <lsftw13@gmail.com>
2014-11-20 12:11:47 -0500

    Added demo settings and made chomper transparent.

--- js/core/settings.js
+++ js/core/settings.js
@@ -22,3 +22,14 @@ var HIGHLIGHT_TILES_WITH_ENTITIES = true; // tiles with entities colored red
 var SHOW_GRID = true; // whether to show grid lines around each tile
 var SHOW_ENEMY_DIRECTION = true; // whether to show enemy path directions on tiles
 var SHOW_ENEMY_SPAWN = true; // whether to show where enemies spawn
+
+// MULTI-TOGGLE
+var DEMO = true;
+
+if (DEMO) {
+    HIGHLIGHT_ENTITY_HITBOXES = false;
+    HIGHLIGHT_TILES_WITH_ENTITIES = false;
+    SHOW_GRID = false;
+    SHOW_ENEMY_DIRECTION = true;
+    SHOW_ENEMY_SPAWN = false;
+}
